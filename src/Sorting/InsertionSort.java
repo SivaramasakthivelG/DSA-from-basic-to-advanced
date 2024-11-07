8b450d397839efe69a1a9df94e99a99a5acb8eac
Sivaramasakthivel Govindaraj <sivaramasakthivel.govindaraj@jasmin-infotech.com>
2024-11-07 12:07:57 +0530

    Hashing1

--- src/Sorting/InsertionSort.java
+++ src/Sorting/InsertionSort.java
@@ -3,25 +3,24 @@ package Sorting;
 public class InsertionSort {
     public static void main(String[] args) {
 
-        int[] arr = new int[]{5, 7, 8, 3, 2, 6,4,1,99,0};
+        int[] arr = new int[]{5, 7, 8, 3, 2, 6, 4, 1, 99, 0};
         int n = arr.length;
         int temp;
 
-        for (int i = 0; i < n-1; i++) {
-            if(arr[i+1]<arr[i]){
-                for(int j=i;j>=0;j--){
-                    if(arr[j+1]<arr[j]) {
-                        temp = arr[j + 1];
-                        arr[j + 1] = arr[j];
-                        arr[j] = temp;
-                    }else{
-                        break;
-                    }
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = i+1; j > 0; j--) {
+                //if the next element less than first element swap
+                if (arr[j] < arr[j - 1]) {
+                    temp = arr[j + 1];
+                    arr[j + 1] = arr[j];
+                    arr[j] = temp;
+                } else {
+                    break;
                 }
             }
         }
 
-        for(int i: arr){
+        for (int i : arr) {
             System.out.println(i);
         }
 
