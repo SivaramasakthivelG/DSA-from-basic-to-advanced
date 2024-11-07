1a22490ed61dea9b2e4676e87aeb2dc7a09cabda
Sivaramasakthivel Govindaraj <sivaramasakthivel.govindaraj@jasmin-infotech.com>
2024-11-07 12:09:27 +0530

    Hashing1

--- src/Sorting/InsertionSort.java
+++ src/Sorting/InsertionSort.java
@@ -8,13 +8,16 @@ public class InsertionSort {
         int temp;
 
         for (int i = 0; i < n - 1; i++) {
-            for (int j = i+1; j > 0; j--) {
+            for (int j = i + 1; j > 0; j--) {
                 //if the next element less than first element swap
                 if (arr[j] < arr[j - 1]) {
-                    temp = arr[j + 1];
-                    arr[j + 1] = arr[j];
+
+                    temp = arr[j - 1];
+                    arr[j - 1] = arr[j];
                     arr[j] = temp;
+
                 } else {
+
                     break;
                 }
             }
